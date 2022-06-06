90f17b891247c2c567cea0f31456c518d4cf1796
Htet Aung Shine <htetaungshine211299@gmail.com>
2022-06-06 11:04:31 +0630

    new project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NewTapPage"
+include(":androidApp")
+include(":shared")
